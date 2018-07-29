ef9968701ca33949c02e4ac142d855ab7568b5ec
nimwijetunga <vnwijetu@uwaterloo.ca>
2018-07-29 17:49:02 -0400

    Changed oauth

--- scripts/push-master.sh
+++ scripts/push-master.sh
@@ -7,12 +7,12 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b master
-  git merge development
-  # git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN_SHELL}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
   git push origin master
 }
 
