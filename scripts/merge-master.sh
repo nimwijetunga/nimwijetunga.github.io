9193cf5442f51433d8d8b8492d6c259fab04a5fc
nimwijetunga <vnwijetu@uwaterloo.ca>
2018-07-29 18:54:03 -0400

    Travis build:  merge

--- scripts/merge-master.sh
+++ scripts/merge-master.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-git remote add origin https://${GH_TOKEN}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
 git checkout master
 git merge development
 git push
---
+++ scripts/push-develop.sh
@@ -0,0 +1,4 @@
+git add .
+git add .travis.yml
+git commit -m "$1"
+git push
