65cd3d5a75d69cdbc768d08965dbdc03bc9a25a3
nimwijetunga <vnwijetu@uwaterloo.ca>
2018-07-29 18:41:18 -0400

    Changed script

--- scripts/push-master.sh
+++ scripts/push-master.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://nimwijetunga:${GH_TOKEN_SHELL}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
   https://github.com/nimwijetunga/nimwijetunga.git
   git push --quiet --set-upstream origin master
 }
