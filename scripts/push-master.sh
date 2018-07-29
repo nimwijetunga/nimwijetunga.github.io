cefcee01a95ca550321ef1a441ad0bbe01b389e5
nimwijetunga <vnwijetu@uwaterloo.ca>
2018-07-29 17:41:22 -0400

    Changing script

--- scripts/push-master.sh
+++ scripts/push-master.sh
@@ -3,16 +3,16 @@
 setup_git() {
   git config --global user.email "vnwijetu@uwaterloo.ca"
   git config --global user.name "nimwijetunga"
-  git remote add origin https://${GH_TOKEN}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
 }
 
 commit_website_files() {
-  git checkout master
+  git checkout -b master
   git merge development
   # git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/nimwijetunga/nimwijetunga.git > /dev/null 2>&1
   git push origin master
 }
 
