f62315325fbf298f7bc0d54ec6d36bd9b4fa6a00
nimwijetunga <vnwijetu@uwaterloo.ca>
2018-07-30 08:29:16 -0400

    changed script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,7 +19,7 @@ git clone --quiet --branch=master https://${GH_TOKEN}@github.com/nimwijetunga/ni
 
 #go into directory and copy data we're interested in to that directory
 cd nimwijetunga
-cp -Rf $HOME/docs/* .
+cp -Rf $HOME/docs/* docs
 
 echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
 echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
