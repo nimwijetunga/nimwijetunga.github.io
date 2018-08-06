0dbddbe9372dc5f80989cd32422e65bc9283c0dc
Travis <travis@travis-ci.org>
2018-08-06 15:11:31 +0000

    Travis build 69

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,28 +7,28 @@ if [[ $TRAVIS_BRANCH == "development" && $TRAVIS_PULL_REQUEST == "false" ]]; the
 echo "Starting to update master\n"
 
 #copy releavent folders
-rsync -av --progress . $HOME/site --exclude node_modules
+# rsync -av --progress . $HOME/site --exclude node_modules #alternative needs more work
 
-# cp -R docs $HOME/docs
-# cp -R backend $HOME/backend
-# cp -R scripts $HOME/scripts
-# cp -R uitests $HOME/uitests
+cp -R docs $HOME/docs
+cp -R backend $HOME/backend
+cp -R scripts $HOME/scripts
+cp -R uitests $HOME/uitests
 
 #go to home and setup git
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-git clone --quiet --branch=master https://${GH_TOKEN}@github.com/nimwijetunga/nimwijetunga.git nimwijetunga > /dev/null
+git clone --quiet --branch=master https://${GH_TOKEN_V2}@github.com/nimwijetunga/nimwijetunga.git nimwijetunga > /dev/null
 
 #copy files to repo
 cd nimwijetunga
-cp -Rf $HOME/site/* .
+# cp -Rf $HOME/site/* 
 
-# cp -Rf $HOME/docs/* docs
-# cp -Rf $HOME/backend/* backend
-# cp -Rf $HOME/scripts/* scripts
-# cp -Rf $HOME/uitests/* uitests
+cp -Rf $HOME/docs/* docs
+cp -Rf $HOME/backend/* backend
+cp -Rf $HOME/scripts/* scripts
+cp -Rf $HOME/uitests/* uitests
 
 #add, commit and push files to master branch
 git add -f .
